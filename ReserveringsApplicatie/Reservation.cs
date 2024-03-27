ca644c990d1f8c4536c38d0166786611ff2ae912
Noahtron40000 <noah@familiedubbelman.nl>
2024-03-27 14:03:25 +0100

    Update Reservation.cs

--- ReserveringsApplicatie/Reservation.cs
+++ ReserveringsApplicatie/Reservation.cs
@@ -8,134 +8,145 @@ namespace ReservationApplication
 
         public void ReservationSystem()
         {
-            bool date_checker = false;
-            bool first_name_checker = false;
-            bool last_name_checker = false;
-            bool phoneNumber_checker = false;
-            bool people_checker = false;
-            bool mail_checker = false;
-            
-            string date = "";
-            int numOfPeople = 0;
-            string name = "";
-            string surname = "";
-            string phoneNumber = "";
-            string email = "";
-            string addition = "";
-
-            while (date_checker != true)
+            bool reservation_confirmation = false
+            while (reservation_confirmation == false)
             {
-                Console.WriteLine("Welkom bij het reserveringsapplicatie van YES!");
-                Console.Write("Voer uw reserveringsdatum in (dd-MM-yyyy): ");
-                date = Console.ReadLine() ?? "";
-                DateTime parsedDate;
-                if (DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out parsedDate))
+                bool date_checker = false;
+                bool first_name_checker = false;
+                bool last_name_checker = false;
+                bool phoneNumber_checker = false;
+                bool people_checker = false;
+                bool mail_checker = false;
+                
+                string date = "";
+                int numOfPeople = 0;
+                string name = "";
+                string surname = "";
+                string phoneNumber = "";
+                string email = "";
+                string addition = "";
+                string reservation_checker = "";
+
+                while (date_checker != true)
                 {
-                    date_checker = true;
+                    Console.WriteLine("Welkom bij het reserveringsapplicatie van YES!");
+                    Console.Write("Voer uw reserveringsdatum in (dd-MM-yyyy): ");
+                    date = Console.ReadLine() ?? "";
+                    DateTime parsedDate;
+                    if (DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out parsedDate))
+                    {
+                        date_checker = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrecte formaat. Probeer: (dd-MM-yyyy)");
+                    }
                 }
-                else
+
+                Console.WriteLine("Wilt u aan het raam? (ja/nee):");
+                bool wantWindow = Console.ReadLine()?.Trim().ToLower() == "ja";
+
+                while (people_checker != true)
                 {
-                    Console.WriteLine("Incorrecte formaat. Probeer: (dd-MM-yyyy)");
+                    Console.Write("Aantal personen: ");
+                    if (int.TryParse(Console.ReadLine(), out numOfPeople) && numOfPeople > 0 && numOfPeople < 48)
+                    {
+                        people_checker = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid aantal personen. Het aantal personen moet tussen 1 en 48 zijn.");
+                    }
+                }
+
+                ReservationSystem rs = new ReservationSystem();
+                rs.ReserveTableForGroup(numOfPeople, wantWindow);
+
+                while (!first_name_checker)
+                {
+                    Console.WriteLine("\nGraag uw contactgegevens achterlaten:");
+                    Console.Write("Voornaam: ");
+                    name = Console.ReadLine() ?? "";
+                    if (!string.IsNullOrWhiteSpace(name))
+                    {
+                        first_name_checker = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Probeer alleen letters te gebruiken.");
+                    }
+                }
+
+                Console.Write("Toevoeging: ");
+                addition = Console.ReadLine() ?? "";
+
+                while (!last_name_checker)
+                {
+                    Console.Write("Achternaam: ");
+                    surname = Console.ReadLine() ?? "";
+                    if (!string.IsNullOrWhiteSpace(surname))
+                    {
+                        last_name_checker = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Probeer alleen letters te gebruiken.");
+                    }
+                }
+
+                while (!phoneNumber_checker)
+                {
+                    Console.Write("Telefoonnummer: ");
+                    phoneNumber = Console.ReadLine() ?? "";
+                    if (phoneNumber.Length == 10 && long.TryParse(phoneNumber, out _))
+                    {
+                        phoneNumber_checker = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Telefoonnummer moet 10 cijfers lang zijn.");
+                    }
+                }
+
+                while (!mail_checker)
+                {
+                    Console.Write("E-mail: ");
+                    email = Console.ReadLine() ?? "";
+                    if (email.Contains("@") && email.Contains("."))
+                    {
+                        mail_checker = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid email. Probeer een echt email in te vullen.");
+                    }
+                }
+
+                Console.WriteLine("\nHeeft u nog opmerkingen of verzoeken?");
+                string comments = Console.ReadLine() ?? "";
+
+                DateTime reservationDate;
+                if(DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out reservationDate))
+                {
+                    db.AddReservation(numOfPeople, name, addition, surname, int.Parse(phoneNumber), email, reservationDate);
+                }
+
+                Console.WriteLine("\nReserveringsgegevens:");
+                Console.WriteLine("Datum: " + date);
+                Console.WriteLine("Aantal Personen: " + numOfPeople);
+                Console.WriteLine("Voornaam: " + name);
+                Console.WriteLine("Toevoering: " + addition);
+                Console.WriteLine("Achternaam: " + surname);
+                Console.WriteLine("Telefoonnummer: " + phoneNumber);
+                Console.WriteLine("E-mail: " + email);
+                Console.WriteLine("Opmerkingen: " + comments);
+                Console.WriteLine("Is dit je gewenste reservatie? (ja/nee)");
+                reservation_checker = Console.ReadLine()?.Trim().ToLower() == "ja";
+                if (reservation_checker == "ja")
+                {
+                    reservation_confirmation = true
                 }
             }
-
-            Console.WriteLine("Wilt u aan het raam? (ja/nee):");
-            bool wantWindow = Console.ReadLine()?.Trim().ToLower() == "ja";
-
-            while (people_checker != true)
-            {
-                Console.Write("Aantal personen: ");
-                if (int.TryParse(Console.ReadLine(), out numOfPeople) && numOfPeople > 0 && numOfPeople < 48)
-                {
-                    people_checker = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid aantal personen. Het aantal personen moet tussen 1 en 48 zijn.");
-                }
-            }
-
-            ReservationSystem rs = new ReservationSystem();
-            rs.ReserveTableForGroup(numOfPeople, wantWindow);
-
-            while (!first_name_checker)
-            {
-                Console.WriteLine("\nGraag uw contactgegevens achterlaten:");
-                Console.Write("Voornaam: ");
-                name = Console.ReadLine() ?? "";
-                if (!string.IsNullOrWhiteSpace(name))
-                {
-                    first_name_checker = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Probeer alleen letters te gebruiken.");
-                }
-            }
-
-            Console.Write("Toevoeging: ");
-            addition = Console.ReadLine() ?? "";
-
-            while (!last_name_checker)
-            {
-                Console.Write("Achternaam: ");
-                surname = Console.ReadLine() ?? "";
-                if (!string.IsNullOrWhiteSpace(surname))
-                {
-                    last_name_checker = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Probeer alleen letters te gebruiken.");
-                }
-            }
-
-            while (!phoneNumber_checker)
-            {
-                Console.Write("Telefoonnummer: ");
-                phoneNumber = Console.ReadLine() ?? "";
-                if (phoneNumber.Length == 10 && long.TryParse(phoneNumber, out _))
-                {
-                    phoneNumber_checker = true;
-                }
-                else
-                {
-                    Console.WriteLine("Telefoonnummer moet 10 cijfers lang zijn.");
-                }
-            }
-
-            while (!mail_checker)
-            {
-                Console.Write("E-mail: ");
-                email = Console.ReadLine() ?? "";
-                if (email.Contains("@") && email.Contains("."))
-                {
-                    mail_checker = true;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid email. Probeer een echt email in te vullen.");
-                }
-            }
-
-            Console.WriteLine("\nHeeft u nog opmerkingen of verzoeken?");
-            string comments = Console.ReadLine() ?? "";
-
-            DateTime reservationDate;
-            if(DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out reservationDate))
-            {
-                db.AddReservation(numOfPeople, name, addition, surname, int.Parse(phoneNumber), email, reservationDate);
-            }
-
-            Console.WriteLine("\nReserveringsgegevens:");
-            Console.WriteLine("Datum: " + date);
-            Console.WriteLine("Aantal Personen: " + numOfPeople);
-            Console.WriteLine("Voornaam: " + name);
-            Console.WriteLine("Toevoering: " + addition);
-            Console.WriteLine("Achternaam: " + surname);
-            Console.WriteLine("Telefoonnummer: " + phoneNumber);
-            Console.WriteLine("E-mail: " + email);
-            Console.WriteLine("Opmerkingen: " + comments);
 
             Console.WriteLine("\nDank u wel! We hopen u gauw te zien bij YES!");
         }
