18048486b0ffca2780df89a9a53e6f1247ba9444
Joey <joeychen0612@hotmail.com>
2024-04-20 02:32:10 +0200

    I may have cooked abit
    Main Menu verbeterd

--- Reserverings-Applicatie/Program.cs
+++ Reserverings-Applicatie/Program.cs
@@ -1,48 +1,81 @@
 ﻿using System;
 using Customer_Reservation_Deleter;
+using System.Collections.Generic;
+using System.Text;
+using System.Console;
 
-namespace ReservationApplication
-{
-    class Program
-    {
-        static void Main(string[] args)
+// namespace ReservationApplication
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Database db = new Database();
+//             db.InitializeDatabase();
+
+//             CRD reservationDeleter = new CRD();
+
+//             string keuze;
+//             do
+//             {
+//                 keuze = Menus.StartUp();
+//                 switch (keuze)
+//                 {
+//                     case "1":
+//                         Menus.AdminLogin();
+//                         break;
+//                     case "2":
+//                         Console.WriteLine("Reserveer een tafel.");
+//                         TestAplicatie reservationApplication = new TestAplicatie();
+//                         reservationApplication.ReservationSystem();
+//                         break;
+//                     case "3":
+//                         Console.WriteLine("Annuleer of bewerk je reservering.");
+//                         reservationDeleter.ReservationDeleter();
+//                         break;
+//                     case "4":
+//                         Console.WriteLine("Toon het menu.");
+//                         FoodMenu foodMenu = new FoodMenu();
+//                         foodMenu.ToonMenu();
+//                         break;
+//                     case "5":
+//                         Console.WriteLine("Fijne dag.");
+//                         break;
+//                     default:
+//                         Console.WriteLine("\nDit is geen geldige optie.");
+//                         break;
+//                 }
+//             } while (keuze != "5");
+//         }
+//     }
+// }
+
+
+namespace {Menu}
+ {
+        class MainMenu
         {
-            Database db = new Database();
-            db.InitializeDatabase();
-
-            CRD reservationDeleter = new CRD();
-
-            string keuze;
-            do
+            public void Start()
             {
-                keuze = Menus.StartUp();
-                switch (keuze)
+                Console.WriteLine("Welkom bij het keuzemenu van YES!");
+
+                Console.KeyInfo keyPressed = ReadKey();
+
+                if (keyPressed.Key == Console.Key.Enter)
                 {
-                    case "1":
-                        Menus.AdminLogin();
-                        break;
-                    case "2":
-                        Console.WriteLine("Reserveer een tafel.");
-                        TestAplicatie reservationApplication = new TestAplicatie();
-                        reservationApplication.ReservationSystem();
-                        break;
-                    case "3":
-                        Console.WriteLine("Annuleer of bewerk je reservering.");
-                        reservationDeleter.ReservationDeleter();
-                        break;
-                    case "4":
-                        Console.WriteLine("Toon het menu.");
-                        FoodMenu foodMenu = new FoodMenu();
-                        foodMenu.ToonMenu();
-                        break;
-                    case "5":
-                        Console.WriteLine("Fijne dag.");
-                        break;
-                    default:
-                        Console.WriteLine("\nDit is geen geldige optie.");
-                        break;
+                    Console.WriteLine("You pressed ENTER");
                 }
-            } while (keuze != "5");
+                else if (keyPressed.Key == Console.Key.UpArrow)
+                {
+                    WriteLine("You pressed the UP ARROW");
+                }
+                else 
+                {
+                    Console.WriteLine("You pressed another key");
+                }
+
+                WriteLine("Press key to exit.");
+                Readkey(true);
+            }
         }
-    }
-}
+ }
